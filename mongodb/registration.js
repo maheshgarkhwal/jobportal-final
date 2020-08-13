5e3ff063d7479dcaba59edfb4007411905ff373a
704priyankamehta <priyanka.mehta@kelltontech.com>
2020-08-13 09:40:32 +0530

    thirtd commit

---
+++ mongodb/registration.js
@@ -0,0 +1,49 @@
+const mongoose  = require("mongoose");
+ var Schema=mongoose.Schema;
+
+var employee=new Schema({
+    name:{
+        type:String,
+        required:true,
+        
+    },
+    email:{
+        type:String,
+        required:true,
+        index:{
+            unique:true
+            }
+        },
+    password:{
+        type:String ,
+        required:true,
+        },
+        phoneno:{
+            type:String,
+            required:true,
+            index:{
+                unique:true
+                }
+        },
+        
+        address:{
+            type:String,
+            required:true,
+            index:{
+                unique:true
+                }
+        
+        },
+        skills:{
+            type:String,
+            required:true
+
+        }
+        
+});
+
+var registration=mongoose.model('registration',employee);
+
+
+
+module.exports=registration;
