3691e31228c1ecc4185f1f7b79a470b0957011a5
704priyankamehta <priyanka.mehta@kelltontech.com>
2020-08-14 17:07:14 +0530

    update

--- routes/employee.js
+++ routes/employee.js
@@ -3,11 +3,11 @@ var data = require("../modules/data");
 var router = express.Router();
 var multer=require('multer');
 var emp=require('../modules/employeedata');
-
+var mail=require("../modules/mail");
 router.put('/updateprofile/:id',data.checkSession, emp.updateprofile);
 router.get('/filter',data.checkSession,emp.filterJobs);
-router.post("/apply",emp.applicationform);
+router.post("/apply",data.checkSession,emp.applicationform);
 
-
+router.post("/mail/:id",mail.email);
 
 module.exports = router;
